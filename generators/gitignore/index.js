0a0f5d7bd79cb35490ea6b655a8d83ebade38306
Kyoichiro Yamada <kyoh86@users.noreply.github.com>
2019-03-04 17:31:28 +0900

    fix

--- generators/gitignore/index.js
+++ generators/gitignore/index.js
@@ -4,7 +4,7 @@ const Generator = require('../../lib/prompt_generator.js');
 
 module.exports = class extends Generator {
   constructor(args, options) {
-    super(args, options, 'gitignore', []);
+    super(args, options, 'gitignore', ['boilerplate']);
   }
 
   prompting() {
